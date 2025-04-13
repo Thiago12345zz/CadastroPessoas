149b802308c0094d804d2cfededbbc28d6d50cb3
Thiago12345zz <thiagobrum@222gmail.com>
2025-04-13 17:03:30 -0300

    Criado a controller

---
+++ src/main/java/br/com/clinicamedica/CadastroPessoas/Controller.java
@@ -0,0 +1,19 @@
+package br.com.clinicamedica.CadastroPessoas;
+
+
+//Falar para o spring que isto é uma anotation, para isto utilizamos @
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasVindas")
+    public String bosaVindas(){
+        return "Essa é minha primeira mensagem nesta rota";
+    }
+
+}
